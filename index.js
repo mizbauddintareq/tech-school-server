0afa08231e26982e303230a2668091b1c0197dcd
mizbauddintareqe <mizbauddintareq@gmail.com>
2022-12-04 13:49:52 +0600

    added fake data

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const courses = require("./data/courses.json");
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const course = courses.find((course) => course.id === id);
+  res.send(course);
 });
 
 app.listen(port, () => {
