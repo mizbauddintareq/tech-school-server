e7c4a76732135bc97f6d15c4617886ccf90e2062
mizbauddintareqe <mizbauddintareq@gmail.com>
2022-12-05 19:44:02 +0600

    finished assignment

--- index.js
+++ index.js
@@ -5,25 +5,39 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+// Courses Data
 const courses = require("./data/courses.json");
 
+// Blogs Data
+const blogs = require("./data/blogs.json");
+
+// Test API
 app.get("/", (req, res) => {
   res.send("hello from the server");
 });
+
+// Get All Courses API
 app.get("/courses", (req, res) => {
   res.send(courses);
 });
 
+// Get Single Courses API
 app.get("/course/:id", (req, res) => {
   const id = req.params.id;
   const course = courses.find((course) => course.id === id);
   res.send(course);
 });
 
+// Private Route API
 app.get("/premium/:id", (req, res) => {
   const id = req.params.id;
   const course = courses.find((course) => course.id === id);
   res.send(course);
+});
+
+// Get All Blogs API
+app.get("/blogs", (req, res) => {
+  res.send(blogs);
 });
 
 app.listen(port, () => {
